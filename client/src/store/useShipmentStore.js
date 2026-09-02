4e0c22c6c3297f991ad38fed18011e5c288b40e5
ay149739-lang <ay149739@gmail.com>
2026-09-02 19:17:59 +0530

    Fix formatting of import statement for fetchRecentShipments

--- client/src/store/useShipmentStore.js
+++ client/src/store/useShipmentStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import {
   fetchShipmentEvents,
   fetchShipmentState,
-  fetchRecentShipments as apiFetchRecent,
+  fetchRecentShipments as  apiFetchRecent,
   appendShipmentEvent
 } from '../lib/api';
 
